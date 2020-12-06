07a75cce794b991273315c6d3c36b47252b2f084
rohitkumarm <rohitkumarrk13568@gmail.com>
2020-12-06 19:12:45 +0530

    feat: added shared library to dev env to don't load same library twice

--- container/src/App.js
+++ container/src/App.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import { mount } from 'marketing/MarketingApp';
-
-console.log(mount);
+import React from "react"
+import MarketingApp from "./components/MarketingApp"
 
 export default () => {
-  return <h1>Hi there!</h1>;
-};
+  return (
+    <>
+      <h1>Hi there!</h1>
+      <hr />
+      <MarketingApp />
+    </>
+  )
+}
